eb85684a267aef7f6c7347391985a1ef9be0de1c
MarcDuMouchel <mardaque@gmail.com>
2018-12-17 11:32:34 -0700

    New RetweetCollection with a few initialization tests woot!

---
+++ RetweetCollection.cpp
@@ -0,0 +1,14 @@
+//
+// Created by CCEClass1 on 12/17/2018.
+//
+
+#include "RetweetCollection.h"
+
+
+bool RetweetCollection::isEmpty() const {
+    return 0 == size();
+}
+
+unsigned int RetweetCollection::size() const {
+    return 0;
+}
